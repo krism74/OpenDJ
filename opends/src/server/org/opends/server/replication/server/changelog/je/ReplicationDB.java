krism74/OpenDJ

commit 1aff01a0d14efd7a9375bd14900a2eacbddeea97

OPENDJ-1217 Servers not synchronized after a conflicting replace on multi-valued attribute on 2 different servers
Fixed regression introduced in r9788 by introducing a similar behaviour as what the code was doing before.

Replica 1 was trying to read a change done on itself and send it to replica 2, but failed to do so.
Hence replica 2 did not receive this UpdateMsg and as a consequence could not resolve it.

Previous code was trying to cursor to a specific CSN in the DB. It could not find it, hence tried to move after it in the DB, but could not do it because the end of the DB was reached. Previous was throwing an exception there, later catching it and then returning an empty cursor.
Newer code removed the useless catch block which let the exception fail this process.
The fix removed throwing the exception and instead returned an empty cursor.



ReplicationDB.java:
In ctor, do not throw an exception, but return an empty cursor instead (Previous code was throwing an exception, catching and ignoring it, then returning an empty cursor).

MessageHandler.java:
Prevented a NullPointerException from being thrown, then caught and ignored.


git-svn-id: https://svn.forgerock.org/opendj/trunk@9887 41b1ffd8-f28e-4786-ab96-9950f0a78031

opends/src/server/org/opends/server/replication/server/changelog/je/ReplicationDB.java
@@ -595,8 +595,11 @@ public class ReplicationDB
           // We could not move the cursor to the expected startCSN
           if (localCursor.getSearchKeyRange(key, data, DEFAULT) != SUCCESS)
           {
-            // We could not even move the cursor close to it => failure
-            throw new ChangelogException(Message.raw("CSN not available"));
+            // We could not even move the cursor close to it
+            // => return empty cursor
+            isClosed = true;
+            cursor = null;
+            return;
           }
 
           // We can move close to the startCSN.
@@ -611,11 +614,6 @@ public class ReplicationDB
         }
         cursor = localCursor;
         cursorHeld = cursor != null;
-      }
-      catch (ChangelogException e)
-      {
-        StaticUtils.close(localCursor);
-        throw e;
       }
       catch (DatabaseException e)
       {

opends/src/server/org/opends/server/replication/server/MessageHandler.java
@@ -459,9 +459,13 @@ public class MessageHandler extends MonitorProvider<MonitorProviderCfg>
     {
       cursor = replicationServerDomain.getCursorFrom(serverState);
       cursor.next();
-      return cursor.getRecord().getCSN();
+      if (cursor.getRecord() != null)
+      {
+        return cursor.getRecord().getCSN();
+      }
+      return null;
     }
-    catch (Exception e)
+    catch (ChangelogException e)
     {
       return null;
     }

commit 5299f44d999b0b30592e599260a49022bcb55c37

Fixing stalled builds. Fixed problem introduced in r9887: an open cursor was never released.
ReplicationDB.java:
In ctors, made sure we always call close on a cursor when it is not held. This will  permanently fix such issues in the ctors.
Other changes are just formatting changes.

git-svn-id: https://svn.forgerock.org/opendj/trunk@9899 41b1ffd8-f28e-4786-ab96-9950f0a78031

opends/src/server/org/opends/server/replication/server/changelog/je/ReplicationDB.java
@@ -55,6 +55,7 @@ import static org.opends.server.util.StaticUtils.*;
 /**
  * This class implements the interface between the underlying database
  * and the JEReplicaDB class.
+ * <p>
  * This is the only class that should have code using the BDB interfaces.
  */
 public class ReplicationDB
@@ -137,7 +138,6 @@ public class ReplicationDB
         replicationServer.getReplicationServerDomain(baseDN, true);
     db = dbenv.getOrAddDb(serverId, baseDN, domain.getGenerationId());
 
-
     intializeCounters();
   }
 
@@ -184,7 +184,6 @@ public class ReplicationDB
     return new CSN(decodeUTF8(data));
   }
 
-
   /**
    * add a list of changes to the underlying db.
    *
@@ -203,8 +202,7 @@ public class ReplicationDB
 
       for (UpdateMsg change : changes)
       {
-        final DatabaseEntry key =
-            createReplicationKey(change.getCSN());
+        final DatabaseEntry key = createReplicationKey(change.getCSN());
         final DatabaseEntry data = new ReplicationData(change);
 
         insertCounterRecordIfNeeded(change.getCSN());
@@ -309,8 +307,6 @@ public class ReplicationDB
     return new ReplServerDBCursor();
   }
 
-
-
   private void closeAndReleaseReadLock(Cursor cursor)
   {
     try
@@ -377,8 +373,6 @@ public class ReplicationDB
       closeAndReleaseReadLock(cursor);
     }
   }
-
-
 
   /**
    * Read the newest CSN present in the database.
@@ -617,14 +611,13 @@ public class ReplicationDB
       }
       catch (DatabaseException e)
       {
-        StaticUtils.close(localCursor);
         throw new ChangelogException(e);
       }
       finally
       {
         if (!cursorHeld)
         {
-          dbCloseLock.readLock().unlock();
+          closeAndReleaseReadLock(localCursor);
         }
       }
     }
@@ -662,13 +655,11 @@ public class ReplicationDB
       }
       catch (ChangelogException e)
       {
-        StaticUtils.close(localCursor);
         abort(localTxn);
         throw e;
       }
       catch (Exception e)
       {
-        StaticUtils.close(localCursor);
         abort(localTxn);
         throw new ChangelogException(e);
       }
@@ -676,7 +667,7 @@ public class ReplicationDB
       {
         if (!cursorHeld)
         {
-          dbCloseLock.readLock().unlock();
+          closeAndReleaseReadLock(localCursor);
         }
       }
     }
@@ -809,7 +800,8 @@ public class ReplicationDB
           {
             return null;
           }
-        } catch (DatabaseException e)
+        }
+        catch (DatabaseException e)
         {
           return null;
         }
@@ -895,12 +887,11 @@ public class ReplicationDB
       // RE-create the db
       db = dbenv.getOrAddDb(serverId, baseDN, -1);
     }
-    catch(Exception e)
+    catch (Exception e)
     {
       MessageBuilder mb = new MessageBuilder();
       mb.append(ERR_ERROR_CLEARING_DB.get(toString(),
-          e.getMessage() + " " +
-          stackTraceToSingleLineString(e)));
+          e.getMessage() + " " + stackTraceToSingleLineString(e)));
       logError(mb.toMessage());
     }
     finally
@@ -909,6 +900,7 @@ public class ReplicationDB
       dbCloseLock.writeLock().unlock();
     }
   }
+
   /**
    * Count the number of changes between 2 changes numbers (inclusive).
    * @param start The lower limit of the count.
@@ -966,9 +958,8 @@ public class ReplicationDB
     return 0;
   }
 
-
-  private void findFirstCounterRecordAfterStartPoint(CSN start,
-      CSN stop, int[] counterValues, int[] distanceToCounterRecords)
+  private void findFirstCounterRecordAfterStartPoint(CSN start, CSN stop,
+      int[] counterValues, int[] distanceToCounterRecords)
       throws DatabaseException
   {
     Cursor cursor = db.openCursor(null, null);
@@ -1020,8 +1011,8 @@ public class ReplicationDB
     }
   }
 
-  private boolean findFirstCounterRecordBeforeStopPoint(CSN start,
-      CSN stop, int[] counterValues, int[] distanceToCounterRecords)
+  private boolean findFirstCounterRecordBeforeStopPoint(CSN start, CSN stop,
+      int[] counterValues, int[] distanceToCounterRecords)
       throws DatabaseException
   {
     Cursor cursor = db.openCursor(null, null);