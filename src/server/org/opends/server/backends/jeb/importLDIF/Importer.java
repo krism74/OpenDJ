krism74/OpenDJ

commit 7cbff8fff8ecf2f3b34dfb3694d252aeb16ad463

Remove dead code
Index.java:
Removed curLocal field (not used anywhere) + removed closeCursor() as a consequence

Importer.java:
Some code cleanup.

git-svn-id: https://svn.forgerock.org/opendj/branches/opendj3-server-dev@11583 41b1ffd8-f28e-4786-ab96-9950f0a78031

src/server/org/opends/server/backends/jeb/importLDIF/Importer.java
@@ -1873,10 +1873,6 @@ public final class Importer implements DiskSpaceMonitorHandler
         }
         else
         {
-          for (Index index : indexMap.values())
-          {
-            index.closeCursor();
-          }
           if (!isCanceled)
           {
             logger.info(NOTE_JEB_IMPORT_LDIF_INDEX_CLOSE, indexMgr.getBufferFileName());
@@ -2361,7 +2357,6 @@ public final class Importer implements DiskSpaceMonitorHandler
           dnKey.setData(key);
           index.insert(dnKey, idSet, dnValue);
         }
-        index.closeCursor();
         if (clearMap)
         {
           map.clear();
@@ -3459,23 +3454,11 @@ public final class Importer implements DiskSpaceMonitorHandler
                 found = true;
                 LocalDBIndexCfg indexCfg = cfg.getLocalDBIndex(idx);
                 SortedSet<IndexType> indexType = indexCfg.getIndexType();
-                if (indexType.contains(EQUALITY))
-                {
-                  indexCount++;
-                }
-                if (indexType.contains(ORDERING))
-                {
-                  indexCount++;
-                }
-                if (indexType.contains(PRESENCE))
-                {
-                  indexCount++;
-                }
-                if (indexType.contains(SUBSTRING))
-                {
-                  indexCount++;
-                }
-                if (indexType.contains(APPROXIMATE))
+                if (indexType.contains(EQUALITY)
+                    || indexType.contains(ORDERING)
+                    || indexType.contains(PRESENCE)
+                    || indexType.contains(SUBSTRING)
+                    || indexType.contains(APPROXIMATE))
                 {
                   indexCount++;
                 }
@@ -3618,7 +3601,7 @@ public final class Importer implements DiskSpaceMonitorHandler
      *
      * @return The total number for entries to process.
      */
-    public long getTotEntries()
+    public long getTotalEntries()
     {
       return this.totalEntries;
     }
@@ -3654,15 +3637,9 @@ public final class Importer implements DiskSpaceMonitorHandler
      * progress report.
      */
     private long previousProcessed;
-
-    /**
-     * The time in milliseconds of the previous progress report.
-     */
+    /** The time in milliseconds of the previous progress report. */
     private long previousTime;
-
-    /**
-     * The environment statistics at the time of the previous report.
-     */
+    /** The environment statistics at the time of the previous report. */
     private EnvironmentStats prevEnvStats;
 
     /**
@@ -3694,12 +3671,12 @@ public final class Importer implements DiskSpaceMonitorHandler
       long deltaCount = entriesProcessed - previousProcessed;
       float rate = 1000f * deltaCount / deltaTime;
       float completed = 0;
-      if (rebuildManager.getTotEntries() > 0)
+      if (rebuildManager.getTotalEntries() > 0)
       {
-        completed = 100f * entriesProcessed / rebuildManager.getTotEntries();
+        completed = 100f * entriesProcessed / rebuildManager.getTotalEntries();
       }
       logger.info(NOTE_JEB_REBUILD_PROGRESS_REPORT, completed, entriesProcessed,
-              rebuildManager.getTotEntries(), rate);
+          rebuildManager.getTotalEntries(), rate);
       try
       {
         Runtime runtime = Runtime.getRuntime();
@@ -3736,26 +3713,16 @@ public final class Importer implements DiskSpaceMonitorHandler
      * progress report.
      */
     private long previousCount;
-
-    /**
-     * The time in milliseconds of the previous progress report.
-     */
+    /** The time in milliseconds of the previous progress report. */
     private long previousTime;
-
-    /**
-     * The environment statistics at the time of the previous report.
-     */
+    /** The environment statistics at the time of the previous report. */
     private EnvironmentStats previousStats;
-
     /** Determines if eviction has been detected. */
     private boolean evicting;
-
     /** Entry count when eviction was detected. */
     private long evictionEntryCount;
 
-    /**
-     * Create a new import progress task.
-     */
+    /** Create a new import progress task. */
     public FirstPhaseProgressTask()
     {
       previousTime = System.currentTimeMillis();
@@ -3769,9 +3736,7 @@ public final class Importer implements DiskSpaceMonitorHandler
       }
     }
 
-    /**
-     * The action to be performed by this timer task.
-     */
+    /** The action to be performed by this timer task. */
     @Override
     public void run()
     {
@@ -3864,20 +3829,12 @@ public final class Importer implements DiskSpaceMonitorHandler
      * progress report.
      */
     private long previousCount;
-
-    /**
-     * The time in milliseconds of the previous progress report.
-     */
+    /** The time in milliseconds of the previous progress report. */
     private long previousTime;
-
-    /**
-     * The environment statistics at the time of the previous report.
-     */
+    /** The environment statistics at the time of the previous report. */
     private EnvironmentStats previousStats;
-
     /** Determines if eviction has been detected. */
     private boolean evicting;
-
     private long latestCount;
 
     /**
@@ -3900,9 +3857,7 @@ public final class Importer implements DiskSpaceMonitorHandler
       }
     }
 
-    /**
-     * The action to be performed by this timer task.
-     */
+    /** The action to be performed by this timer task. */
     @Override
     public void run()
     {

src/server/org/opends/server/backends/jeb/Index.java
@@ -22,9 +22,13 @@
  *
  *
  *      Copyright 2006-2010 Sun Microsystems, Inc.
- *      Portions Copyright 2012-2014 ForgeRock AS
+ *      Portions Copyright 2012-2015 ForgeRock AS
  */
 package org.opends.server.backends.jeb;
+
+import static com.sleepycat.je.OperationStatus.*;
+
+import static org.opends.messages.JebMessages.*;
 
 import java.util.*;
 
@@ -41,10 +45,6 @@ import org.opends.server.types.Modification;
 import org.opends.server.util.StaticUtils;
 
 import com.sleepycat.je.*;
-
-import static com.sleepycat.je.OperationStatus.*;
-
-import static org.opends.messages.JebMessages.*;
 
 /**
  * Represents an index implemented by a JE database in which each key maps to
@@ -109,8 +109,6 @@ public class Index extends DatabaseContainer
    */
   private boolean rebuildRunning;
 
-  /** Thread local area to store per thread cursors. */
-  private final ThreadLocal<Cursor> curLocal = new ThreadLocal<Cursor>();
   private final ImportIDSet newImportIDSet;
 
   /**
@@ -172,7 +170,15 @@ public class Index extends DatabaseContainer
     getBufferedIndexValues(buffer, keyBytes).addEntryID(keyBytes, entryID);
   }
 
-  private void deleteKey(DatabaseEntry key, ImportIDSet importIdSet, DatabaseEntry data) throws DatabaseException {
+  /**
+   * Delete the specified import ID set from the import ID set associated with the key.
+   *
+   * @param key The key to delete the set from.
+   * @param importIdSet The import ID set to delete.
+   * @param data A database entry to use for data.
+   * @throws DatabaseException If a database error occurs.
+   */
+  public void delete(DatabaseEntry key, ImportIDSet importIdSet, DatabaseEntry data) throws DatabaseException {
     final OperationStatus status = read(null, key, data, LockMode.DEFAULT);
     if(status == SUCCESS) {
       newImportIDSet.clear(false);
@@ -192,7 +198,15 @@ public class Index extends DatabaseContainer
     }
   }
 
-  private void insertKey(DatabaseEntry key, ImportIDSet importIdSet, DatabaseEntry data) throws DatabaseException {
+  /**
+   * Insert the specified import ID set into this index. Creates a DB cursor if needed.
+   *
+   * @param key The key to add the set to.
+   * @param importIdSet The set of import IDs.
+   * @param data Database entry to reuse for read.
+   * @throws DatabaseException If a database error occurs.
+   */
+  public void insert(DatabaseEntry key, ImportIDSet importIdSet, DatabaseEntry data) throws DatabaseException {
     final OperationStatus status = read(null, key, data, LockMode.DEFAULT);
     if(status == OperationStatus.SUCCESS) {
       newImportIDSet.clear(false);
@@ -212,43 +226,6 @@ public class Index extends DatabaseContainer
       // Should never happen during import.
       throw new RuntimeException();
     }
-  }
-
-  /**
-   * Insert the specified import ID set into this index. Creates a DB
-   * cursor if needed.
-   *
-   * @param key The key to add the set to.
-   * @param importIdSet The set of import IDs.
-   * @param data Database entry to reuse for read.
-   * @throws DatabaseException If a database error occurs.
-   */
-  public void insert(DatabaseEntry key, ImportIDSet importIdSet, DatabaseEntry data) throws DatabaseException {
-    Cursor cursor = curLocal.get();
-    if(cursor == null) {
-      cursor = openCursor(null, null);
-      curLocal.set(cursor);
-    }
-    insertKey(key, importIdSet, data);
-  }
-
-  /**
-   * Delete the specified import ID set from the import ID set associated with
-   * the key.
-   *
-   * @param key The key to delete the set from.
-   * @param importIdSet The import ID set to delete.
-   * @param data A database entry to use for data.
-   *
-   * @throws DatabaseException If a database error occurs.
-   */
-  public void delete(DatabaseEntry key, ImportIDSet importIdSet, DatabaseEntry data) throws DatabaseException {
-    Cursor cursor = curLocal.get();
-    if(cursor == null) {
-      cursor = openCursor(null, null);
-      curLocal.set(cursor);
-    }
-    deleteKey(key, importIdSet, data);
   }
 
   /**
@@ -724,19 +701,6 @@ public class Index extends DatabaseContainer
   public int getEntryLimitExceededCount()
   {
     return entryLimitExceededCount;
-  }
-
-  /**
-   * Close any cursors open against this index.
-   *
-   * @throws DatabaseException  If a database error occurs.
-   */
-  public void closeCursor() throws DatabaseException {
-    Cursor cursor = curLocal.get();
-    if(cursor != null) {
-      cursor.close();
-      curLocal.remove();
-    }
   }
 
   /**

src/server/org/opends/server/backends/pluggable/Index.java
@@ -106,9 +106,6 @@ public class Index extends DatabaseContainer
    */
   private boolean rebuildRunning;
 
-  /** Thread local area to store per thread cursors. */
-  private final ThreadLocal<Cursor> curLocal = new ThreadLocal<Cursor>();
-
   /**
    * Create a new index object.
    * @param name The name of the index database within the entryContainer.
@@ -588,19 +585,6 @@ public class Index extends DatabaseContainer
   public int getEntryLimitExceededCount()
   {
     return entryLimitExceededCount;
-  }
-
-  /**
-   * Close any cursors open against this index.
-   *
-   * @throws StorageRuntimeException  If a database error occurs.
-   */
-  public void closeCursor() throws StorageRuntimeException {
-    Cursor cursor = curLocal.get();
-    if(cursor != null) {
-      cursor.close();
-      curLocal.remove();
-    }
   }
 
   /**

commit 24809cdedfd275813ace874c8dd7a5af61ca9cd3

Importer.java: In getRebuildListIndexCount(), code cleanup: - extracted methods - fixed a bug I introduced in r11583, where I collapsed several ifs into one.
git-svn-id: https://svn.forgerock.org/opendj/branches/opendj3-server-dev@11590 41b1ffd8-f28e-4786-ab96-9950f0a78031

src/server/org/opends/server/backends/jeb/importLDIF/Importer.java
@@ -3377,50 +3377,43 @@ public final class Importer implements DiskSpaceMonitorHandler
         else if ("id2subtree".equals(lowerName)
             || "id2children".equals(lowerName))
         {
-          LocalizableMessage msg = ERR_JEB_ATTRIBUTE_INDEX_NOT_CONFIGURED.get(index);
-          throw new InitializationException(msg);
+          throw attributeIndexNotConfigured(index);
         }
         else
         {
-          String[] attrIndexParts = lowerName.split("\\.");
+          final String[] attrIndexParts = lowerName.split("\\.");
           if (attrIndexParts.length <= 0 || attrIndexParts.length > 3)
           {
-            LocalizableMessage msg = ERR_JEB_ATTRIBUTE_INDEX_NOT_CONFIGURED.get(index);
-            throw new InitializationException(msg);
+            throw attributeIndexNotConfigured(index);
           }
           AttributeType attrType = DirectoryServer.getAttributeType(attrIndexParts[0]);
           if (attrType == null)
           {
-            LocalizableMessage msg = ERR_JEB_ATTRIBUTE_INDEX_NOT_CONFIGURED.get(index);
-            throw new InitializationException(msg);
+            throw attributeIndexNotConfigured(index);
           }
           if (attrIndexParts.length != 1)
           {
-            String indexType = attrIndexParts[1];
+            final String indexType = attrIndexParts[1];
             if (attrIndexParts.length == 2)
             {
               if ("presence".equals(indexType)
                   || "equality".equals(indexType)
-                  || "substring".equals(indexType)
                   || "ordering".equals(indexType)
+                  || "substring".equals(indexType)
                   || "approximate".equals(indexType))
               {
                 indexCount++;
               }
               else
               {
-                LocalizableMessage msg =
-                    ERR_JEB_ATTRIBUTE_INDEX_NOT_CONFIGURED.get(index);
-                throw new InitializationException(msg);
+                throw attributeIndexNotConfigured(index);
               }
             }
-            else
+            else // attrIndexParts.length == 3
             {
               if (!findExtensibleMatchingRule(cfg, indexType + "." + attrIndexParts[2]))
               {
-                LocalizableMessage msg =
-                    ERR_JEB_ATTRIBUTE_INDEX_NOT_CONFIGURED.get(index);
-                throw new InitializationException(msg);
+                throw attributeIndexNotConfigured(index);
               }
               indexCount++;
             }
@@ -3430,49 +3423,28 @@ public final class Importer implements DiskSpaceMonitorHandler
             boolean found = false;
             for (final String idx : cfg.listLocalDBIndexes())
             {
-              if (!idx.equalsIgnoreCase(index))
+              if (idx.equalsIgnoreCase(index))
               {
-                continue;
-              }
-              found = true;
-              LocalDBIndexCfg indexCfg = cfg.getLocalDBIndex(idx);
-              SortedSet<IndexType> indexType = indexCfg.getIndexType();
-              if (indexType.contains(EQUALITY)
-                  || indexType.contains(ORDERING)
-                  || indexType.contains(PRESENCE)
-                  || indexType.contains(SUBSTRING)
-                  || indexType.contains(APPROXIMATE))
-              {
-                indexCount++;
-              }
-              if (indexType.contains(EXTENSIBLE))
-              {
-                Set<String> extensibleRules = indexCfg.getIndexExtensibleMatchingRule();
-                boolean shared = false;
-                for (final String exRule : extensibleRules)
-                {
-                  if (exRule.endsWith(".sub"))
-                  {
-                    indexCount++;
-                  }
-                  else if (!shared)
-                  {
-                    shared = true;
-                    indexCount++;
-                  }
-                }
+                found = true;
+                final LocalDBIndexCfg indexCfg = cfg.getLocalDBIndex(idx);
+                indexCount += getAttributeIndexCount(indexCfg.getIndexType(),
+                    PRESENCE, EQUALITY, ORDERING, SUBSTRING, APPROXIMATE);
+                indexCount += getExtensibleIndexCount(indexCfg);
               }
             }
             if (!found)
             {
-              LocalizableMessage msg =
-                  ERR_JEB_ATTRIBUTE_INDEX_NOT_CONFIGURED.get(index);
-              throw new InitializationException(msg);
+              throw attributeIndexNotConfigured(index);
             }
           }
         }
       }
       return indexCount;
+    }
+
+    private InitializationException attributeIndexNotConfigured(String index)
+    {
+      return new InitializationException(ERR_JEB_ATTRIBUTE_INDEX_NOT_CONFIGURED.get(index));
     }
 
     private boolean findExtensibleMatchingRule(LocalDBBackendCfg cfg, String indexExRuleName) throws ConfigException
@@ -3492,6 +3464,41 @@ public final class Importer implements DiskSpaceMonitorHandler
         }
       }
       return false;
+    }
+
+    private int getAttributeIndexCount(SortedSet<IndexType> indexTypes, IndexType... toFinds)
+    {
+      int result = 0;
+      for (IndexType toFind : toFinds)
+      {
+        if (indexTypes.contains(toFind))
+        {
+          result++;
+        }
+      }
+      return result;
+    }
+
+    private int getExtensibleIndexCount(LocalDBIndexCfg indexCfg)
+    {
+      int result = 0;
+      if (indexCfg.getIndexType().contains(EXTENSIBLE))
+      {
+        boolean shared = false;
+        for (final String exRule : indexCfg.getIndexExtensibleMatchingRule())
+        {
+          if (exRule.endsWith(".sub"))
+          {
+            result++;
+          }
+          else if (!shared)
+          {
+            shared = true;
+            result++;
+          }
+        }
+      }
+      return result;
     }
 
     private void processEntry(Entry entry, EntryID entryID)