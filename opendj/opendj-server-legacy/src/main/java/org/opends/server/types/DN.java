krism74/OpenDJ

commit f9603bad6ef1de02b0878fcfc927ab019bb186d6

AutoRefactor: no assignment in if statement
git-svn-id: https://svn.forgerock.org/opendj/trunk@11759 41b1ffd8-f28e-4786-ab96-9950f0a78031

opendj/opendj-server-legacy/src/main/java/org/opends/server/types/DN.java
@@ -1525,8 +1525,8 @@ public final class DN implements Comparable<DN>, Serializable
 
       int namePos = 0;
       int nameLength = nameBytes.length();
-      byte ch = nameBytes.byteAt(0);
-      if ((ch == 'o') || (ch == 'O'))
+      byte ch0 = nameBytes.byteAt(0);
+      if (ch0 == 'o' || ch0 == 'O')
       {
         if (nameLength <= 4)
         {
@@ -1534,11 +1534,11 @@ public final class DN implements Comparable<DN>, Serializable
         }
         else
         {
-          if ((((ch = nameBytes.byteAt(1)) == 'i') ||
-               (ch == 'I')) &&
-              (((ch = nameBytes.byteAt(2)) == 'd') ||
-               (ch == 'D')) &&
-              (nameBytes.byteAt(3) == '.'))
+          byte ch1 = nameBytes.byteAt(1);
+          byte ch2 = nameBytes.byteAt(1);
+          if ((ch1 == 'i' || ch1 == 'I')
+              && (ch2 == 'd' || ch2 == 'D')
+              && nameBytes.byteAt(3) == '.')
           {
             nameBytes = nameBytes.subSequence(4, nameBytes.length());
             nameLength -= 4;
@@ -1552,7 +1552,7 @@ public final class DN implements Comparable<DN>, Serializable
 
       while (validOID && (namePos < nameLength))
       {
-        ch = nameBytes.byteAt(namePos++);
+        byte ch = nameBytes.byteAt(namePos++);
         if (isDigit((char)ch))
         {
           while (validOID && (namePos < nameLength) &&
@@ -1930,8 +1930,8 @@ public final class DN implements Comparable<DN>, Serializable
 
       int namePos = 0;
       int nameLength = attributeName.length();
-      char ch = attributeName.charAt(0);
-      if ((ch == 'o') || (ch == 'O'))
+      char ch0 = attributeName.charAt(0);
+      if (ch0 == 'o' || ch0 == 'O')
       {
         if (nameLength <= 4)
         {
@@ -1939,11 +1939,11 @@ public final class DN implements Comparable<DN>, Serializable
         }
         else
         {
-          if ((((ch = attributeName.charAt(1)) == 'i') ||
-               (ch == 'I')) &&
-              (((ch = attributeName.charAt(2)) == 'd') ||
-               (ch == 'D')) &&
-              (attributeName.charAt(3) == '.'))
+          char ch1 = attributeName.charAt(1);
+          char ch2 = attributeName.charAt(2);
+          if ((ch1 == 'i' || ch1 == 'I')
+              && (ch2 == 'd' || ch2 == 'D')
+              && attributeName.charAt(3) == '.')
           {
             attributeName.delete(0, 4);
             nameLength -= 4;
@@ -1957,7 +1957,7 @@ public final class DN implements Comparable<DN>, Serializable
 
       while (validOID && (namePos < nameLength))
       {
-        ch = attributeName.charAt(namePos++);
+        char ch = attributeName.charAt(namePos++);
         if (isDigit(ch))
         {
           while (validOID && (namePos < nameLength) &&

opendj/opendj-server-legacy/src/main/java/org/opends/admin/ads/ADSContext.java
@@ -1552,9 +1552,9 @@ public class ADSContext
    */
   private static LdapName makeDNFromServerProperties(
       Map<ServerProperty, Object> serverProperties) throws ADSContextException
-      {
-    String serverID ;
-    if ( (serverID = getServerID(serverProperties)) != null )
+  {
+    String serverID = getServerID(serverProperties) ;
+    if (serverID != null)
     {
       return makeDNFromServerUniqueId(serverID);
     }
@@ -1570,7 +1570,7 @@ public class ADSContext
       ServerDescriptor s = ServerDescriptor.createStandalone(serverProperties);
       return makeDNFromServerUniqueId(s.getHostPort(true));
     }
-      }
+  }
 
   /**
    * This method returns the DN of the entry that corresponds to the given

opendj/opendj-server-legacy/src/main/java/org/opends/server/authorization/dseecompat/AciListenerManager.java
@@ -243,11 +243,9 @@ public class AciListenerManager implements
     private void doPostAdd(Entry addedEntry)
     {
       // This entry might have both global and aci attribute types.
-      boolean hasAci, hasGlobalAci = false;
-      if ((hasAci = addedEntry
-          .hasOperationalAttribute(AciHandler.aciType))
-          || (hasGlobalAci = addedEntry
-              .hasAttribute(AciHandler.globalAciType)))
+      boolean hasAci = addedEntry.hasOperationalAttribute(AciHandler.aciType);
+      boolean hasGlobalAci = addedEntry.hasAttribute(AciHandler.globalAciType);
+      if (hasAci || hasGlobalAci)
       {
         // Ignore this list, the ACI syntax has already passed and it
         // should be empty.

opendj/opendj-server-legacy/src/main/java/org/opends/server/authorization/dseecompat/ParentInheritance.java
@@ -226,11 +226,12 @@ public class ParentInheritance {
      * @return The attribute type.
      */
     public AttributeType getAttributeType() {
-      AttributeType attrType;
-      if((attrType =
-           DirectoryServer.getAttributeType(attrTypeStr.toLowerCase())) == null)
-        attrType=
-             DirectoryServer.getDefaultAttributeType(attrTypeStr.toLowerCase());
+      final String attrName = attrTypeStr.toLowerCase();
+      AttributeType attrType = DirectoryServer.getAttributeType(attrName);
+      if(attrType == null)
+      {
+        attrType = DirectoryServer.getDefaultAttributeType(attrName);
+      }
       return attrType;
     }
 

opendj/opendj-server-legacy/src/main/java/org/opends/server/authorization/dseecompat/PatternDN.java
@@ -995,8 +995,8 @@ public class PatternDN
 
       int namePos = 0;
       int nameLength = attributeName.length();
-      char ch = attributeName.charAt(0);
-      if ((ch == 'o') || (ch == 'O'))
+      char ch0 = attributeName.charAt(0);
+      if (ch0 == 'o' || ch0 == 'O')
       {
         if (nameLength <= 4)
         {
@@ -1004,11 +1004,11 @@ public class PatternDN
         }
         else
         {
-          if ((((ch = attributeName.charAt(1)) == 'i') ||
-               (ch == 'I')) &&
-              (((ch = attributeName.charAt(2)) == 'd') ||
-               (ch == 'D')) &&
-              (attributeName.charAt(3) == '.'))
+          char ch1 = attributeName.charAt(1);
+          char ch2 = attributeName.charAt(2);
+          if ((ch1 == 'i' || ch1 == 'I')
+              && (ch2 == 'd' || ch2 == 'D')
+              && attributeName.charAt(3) == '.')
           {
             attributeName.delete(0, 4);
             nameLength -= 4;
@@ -1022,7 +1022,7 @@ public class PatternDN
 
       while (validOID && (namePos < nameLength))
       {
-        ch = attributeName.charAt(namePos++);
+        char ch = attributeName.charAt(namePos++);
         if (isDigit(ch))
         {
           while (validOID && (namePos < nameLength) &&

opendj/opendj-server-legacy/src/main/java/org/opends/server/authorization/dseecompat/Permission.java
@@ -74,8 +74,8 @@ public class Permission {
      */
     private Permission(String accessType, String rights)
     throws AciException {
-        if ((this.accessType =
-            EnumAccessType.decode(accessType)) == null){
+        this.accessType = EnumAccessType.decode(accessType);
+        if (this.accessType == null){
             LocalizableMessage message =
                 WARN_ACI_SYNTAX_INVALID_ACCESS_TYPE_VERSION.get(accessType);
             throw new AciException(message);

opendj/opendj-server-legacy/src/main/java/org/opends/server/authorization/dseecompat/TargAttrFilterList.java
@@ -131,16 +131,15 @@ public class TargAttrFilterList {
             if (semicolon != -1)
                 attributeName=attributeName.substring(0, semicolon);
             String filterString=matcher.group(filterPos);
-            AttributeType attributeType;
-            if((attributeType =
-                    DirectoryServer.getAttributeType(attributeName)) == null)
-                attributeType =
-                        DirectoryServer.getDefaultAttributeType(attributeName);
+            AttributeType attrType = DirectoryServer.getAttributeType(attributeName);
+            if (attrType == null) {
+                attrType = DirectoryServer.getDefaultAttributeType(attributeName);
+            }
             SearchFilter filter;
             //Check if it is a valid filter and add it to the list map if ok.
             try {
                filter = SearchFilter.createFilterFromString(filterString);
-               attrFilterList.put(attributeType, filter);
+               attrFilterList.put(attrType, filter);
             } catch (DirectoryException ex) {
                 LocalizableMessage er=ex.getMessageObject();
                 LocalizableMessage message =
@@ -149,9 +148,8 @@ public class TargAttrFilterList {
                 throw new AciException(message);
             }
             //Verify the filter components. This check assures that each
-            //attribute type in the filter matches the provided attribute
-            //type.
-            verifyFilterComponents(filter, attributeType);
+            //attribute type in the filter matches the provided attribute type.
+            verifyFilterComponents(filter, attrType);
         }
         return new TargAttrFilterList(mask, attrFilterList);
     }

opendj/opendj-server-legacy/src/main/java/org/opends/server/authorization/dseecompat/TargetAttr.java
@@ -150,15 +150,14 @@ public class TargetAttr {
                     throw new AciException(message);
                 }
             } else {
-                AttributeType attributeType;
-                if((attributeType =
-                        DirectoryServer.getAttributeType(attribute)) == null)
-                    attributeType =
-                            DirectoryServer.getDefaultAttributeType(attribute);
-                if(attributeType.isOperational())
-                    opAttributes.add(attributeType);
+                AttributeType attrType = DirectoryServer.getAttributeType(attribute);
+                if (attrType == null) {
+                    attrType = DirectoryServer.getDefaultAttributeType(attribute);
+                }
+                if(attrType.isOperational())
+                    opAttributes.add(attrType);
                 else
-                    attributes.add(attributeType);
+                    attributes.add(attrType);
             }
         }
     }

commit 0d8c72a76c34d31d7e03673df5a09e1e1496302b

Fix problem introduced in r11759
git-svn-id: https://svn.forgerock.org/opendj/trunk@11770 41b1ffd8-f28e-4786-ab96-9950f0a78031

opendj/opendj-server-legacy/src/main/java/org/opends/server/types/DN.java
@@ -26,6 +26,11 @@
  */
 package org.opends.server.types;
 
+import static org.forgerock.util.Reject.*;
+import static org.opends.messages.SchemaMessages.*;
+import static org.opends.server.config.ConfigConstants.*;
+import static org.opends.server.util.StaticUtils.*;
+
 import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
@@ -39,11 +44,6 @@ import org.forgerock.opendj.ldap.ResultCode;
 import org.forgerock.opendj.ldap.SearchScope;
 import org.forgerock.util.Reject;
 import org.opends.server.core.DirectoryServer;
-
-import static org.forgerock.util.Reject.*;
-import static org.opends.messages.SchemaMessages.*;
-import static org.opends.server.config.ConfigConstants.*;
-import static org.opends.server.util.StaticUtils.*;
 
 /**
  * This class defines a data structure for storing and interacting
@@ -1535,7 +1535,7 @@ public final class DN implements Comparable<DN>, Serializable
         else
         {
           byte ch1 = nameBytes.byteAt(1);
-          byte ch2 = nameBytes.byteAt(1);
+          byte ch2 = nameBytes.byteAt(2);
           if ((ch1 == 'i' || ch1 == 'I')
               && (ch2 == 'd' || ch2 == 'D')
               && nameBytes.byteAt(3) == '.')