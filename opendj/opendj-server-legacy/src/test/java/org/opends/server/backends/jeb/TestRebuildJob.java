krism74/OpenDJ

commit eb51f993333e3bb03f06cee3cc22d2aabc04227f

Pass ServerContext object down method calls to the pluggable backend Importer class.
git-svn-id: https://svn.forgerock.org/opendj/trunk@11987 41b1ffd8-f28e-4786-ab96-9950f0a78031

opendj/opendj-server-legacy/src/test/java/org/opends/server/backends/jeb/TestRebuildJob.java
@@ -161,8 +161,7 @@ public class TestRebuildJob extends JebTestCase
     RebuildConfig rebuildConfig = new RebuildConfig();
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex(index);
-    backend = DirectoryServer.getBackend(backendID);
-    backend.rebuildBackend(rebuildConfig);
+    rebuildBackend(backendID, rebuildConfig);
 
     if(index.contains(".") && !index.startsWith("vlv."))
     {
@@ -188,8 +187,7 @@ public class TestRebuildJob extends JebTestCase
     RebuildConfig rebuildConfig = new RebuildConfig();
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex("id2entry");
-    backend = DirectoryServer.getBackend(backendID);
-    backend.rebuildBackend(rebuildConfig);
+    rebuildBackend(backendID, rebuildConfig);
 
   }
 
@@ -207,11 +205,10 @@ public class TestRebuildJob extends JebTestCase
     RebuildConfig rebuildConfig = new RebuildConfig();
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex("id2entry");
-    backend = DirectoryServer.getBackend(backendID);
     TaskUtils.disableBackend(backendID);
 
     try {
-      backend.rebuildBackend(rebuildConfig);
+      rebuildBackend(backendID, rebuildConfig);
     } finally {
       TaskUtils.enableBackend(backendID);
     }
@@ -224,8 +221,7 @@ public class TestRebuildJob extends JebTestCase
     RebuildConfig rebuildConfig = new RebuildConfig();
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex(index);
-    backend = DirectoryServer.getBackend(backendID);
-    backend.rebuildBackend(rebuildConfig);
+    rebuildBackend(backendID, rebuildConfig);
   }
 
   @Test(dataProvider = "systemIndexes",
@@ -236,8 +232,7 @@ public class TestRebuildJob extends JebTestCase
     RebuildConfig rebuildConfig = new RebuildConfig();
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex(index);
-    backend = DirectoryServer.getBackend(backendID);
-    backend.rebuildBackend(rebuildConfig);
+    rebuildBackend(backendID, rebuildConfig);
   }
 
   @Test(dataProvider = "systemIndexes")
@@ -248,9 +243,8 @@ public class TestRebuildJob extends JebTestCase
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex(index);
 
-    backend = DirectoryServer.getBackend(backendID);
     TaskUtils.disableBackend(backendID);
-    backend.rebuildBackend(rebuildConfig);
+    rebuildBackend(backendID, rebuildConfig);
 
     //TODO: Verify dn2uri database as well.
     if (!"dn2uri".equalsIgnoreCase(index))
@@ -296,17 +290,22 @@ public class TestRebuildJob extends JebTestCase
 
   private void rebuildIndexes(RebuildConfig rebuildConfig) throws Exception
   {
-    backend = DirectoryServer.getBackend(backendID);
     TaskUtils.disableBackend(backendID);
     try
     {
-      backend.rebuildBackend(rebuildConfig);
+      rebuildBackend(backendID, rebuildConfig);
       assertEquals(verifyBackend(null), 0);
     }
     finally
     {
       TaskUtils.enableBackend(backendID);
     }
+  }
+
+  private void rebuildBackend(String backendID, RebuildConfig rebuildConfig) throws Exception
+  {
+    backend = DirectoryServer.getBackend(backendID);
+    backend.rebuildBackend(rebuildConfig, DirectoryServer.getInstance().getServerContext());
   }
 
   @Test

opendj/opendj-server-legacy/src/main/java/org/opends/guitools/controlpanel/util/ReadOnlyConfigFileHandler.java
@@ -346,8 +346,8 @@ public class ReadOnlyConfigFileHandler extends ConfigHandler<BackendCfg>
 
   /** {@inheritDoc} */
   @Override
-  public LDIFImportResult importLDIF(LDIFImportConfig arg0)
-  throws DirectoryException
+  public LDIFImportResult importLDIF(LDIFImportConfig importConfig, ServerContext serverContext)
+      throws DirectoryException
   {
     return null;
   }

opendj/opendj-server-legacy/src/main/java/org/opends/server/api/Backend.java
@@ -637,15 +637,13 @@ public abstract class Backend<C extends Configuration>
    * returns {@code true}.  Note that the server will not explicitly
    * initialize this backend before calling this method.
    *
-   * @param  importConfig  The configuration to use when performing
-   *                       the import.
-   *
+   * @param  importConfig  The configuration to use when performing the import.
+   * @param  serverContext The server context
    * @return  Information about the result of the import processing.
-   *
    * @throws  DirectoryException  If a problem occurs while performing
    *                              the LDIF import.
    */
-  public abstract LDIFImportResult importLDIF(LDIFImportConfig importConfig)
+  public abstract LDIFImportResult importLDIF(LDIFImportConfig importConfig, ServerContext serverContext)
          throws DirectoryException;
 
   /**
@@ -683,8 +681,8 @@ public abstract class Backend<C extends Configuration>
    * @throws DirectoryException
    *           If a Directory Server error occurs.
    */
-  public void rebuildBackend(RebuildConfig rebuildConfig) throws InitializationException, ConfigException,
-      DirectoryException
+  public void rebuildBackend(RebuildConfig rebuildConfig, ServerContext serverContext)
+      throws InitializationException, ConfigException, DirectoryException
   {
     throw new DirectoryException(ResultCode.UNWILLING_TO_PERFORM,
         ERR_INDEXES_NOT_SUPPORTED.get(getBackendID()));

opendj/opendj-server-legacy/src/main/java/org/opends/server/backends/BackupBackend.java
@@ -934,8 +934,8 @@ public class BackupBackend
 
   /** {@inheritDoc} */
   @Override
-  public LDIFImportResult importLDIF(LDIFImportConfig importConfig)
-         throws DirectoryException
+  public LDIFImportResult importLDIF(LDIFImportConfig importConfig, ServerContext serverContext)
+      throws DirectoryException
   {
     throw new DirectoryException(ResultCode.UNWILLING_TO_PERFORM,
         ERR_BACKEND_IMPORT_AND_EXPORT_NOT_SUPPORTED.get(getBackendID()));

opendj/opendj-server-legacy/src/main/java/org/opends/server/backends/ChangelogBackend.java
@@ -622,7 +622,7 @@ public class ChangelogBackend extends Backend<Configuration>
 
   /** {@inheritDoc} */
   @Override
-  public LDIFImportResult importLDIF(LDIFImportConfig importConfig)
+  public LDIFImportResult importLDIF(LDIFImportConfig importConfig, ServerContext serverContext)
       throws DirectoryException
   {
     throw new DirectoryException(ResultCode.UNWILLING_TO_PERFORM,

opendj/opendj-server-legacy/src/main/java/org/opends/server/backends/LDIFBackend.java
@@ -1033,13 +1033,11 @@ public class LDIFBackend
 
   /** {@inheritDoc} */
   @Override
-  public LDIFImportResult importLDIF(LDIFImportConfig importConfig)
-         throws DirectoryException
+  public LDIFImportResult importLDIF(LDIFImportConfig importConfig, ServerContext serverContext)
+      throws DirectoryException
   {
     return importLDIF(importConfig, true);
   }
-
-
 
   /**
    * Processes an LDIF import operation, optionally writing the resulting LDIF

opendj/opendj-server-legacy/src/main/java/org/opends/server/backends/MemoryBackend.java
@@ -668,8 +668,8 @@ public class MemoryBackend
 
   /** {@inheritDoc} */
   @Override
-  public synchronized LDIFImportResult importLDIF(LDIFImportConfig importConfig)
-         throws DirectoryException
+  public synchronized LDIFImportResult importLDIF(LDIFImportConfig importConfig, ServerContext serverContext)
+      throws DirectoryException
   {
     clearMemoryBackend();
 

opendj/opendj-server-legacy/src/main/java/org/opends/server/backends/MonitorBackend.java
@@ -405,7 +405,7 @@ public class MonitorBackend extends Backend<MonitorBackendCfg> implements
 
   /** {@inheritDoc} */
   @Override
-  public LDIFImportResult importLDIF(final LDIFImportConfig importConfig)
+  public LDIFImportResult importLDIF(final LDIFImportConfig importConfig, ServerContext serverContext)
       throws DirectoryException
   {
     throw new DirectoryException(ResultCode.UNWILLING_TO_PERFORM,

commit 165407e6cb4c5aaf189cb5b40ffca0e46ebe3205

Fix build break introduced by r11987
git-svn-id: https://svn.forgerock.org/opendj/trunk@11998 41b1ffd8-f28e-4786-ab96-9950f0a78031

opendj/opendj-server-legacy/src/test/java/org/opends/server/backends/jeb/TestRebuildJob.java
@@ -34,6 +34,7 @@ import org.opends.server.backends.RebuildConfig;
 import org.opends.server.backends.RebuildConfig.RebuildMode;
 import org.opends.server.backends.VerifyConfig;
 import org.opends.server.core.DirectoryServer;
+import org.opends.server.core.ServerContext;
 import org.opends.server.tasks.TaskUtils;
 import org.opends.server.types.DN;
 import org.opends.server.types.DirectoryException;
@@ -161,7 +162,8 @@ public class TestRebuildJob extends JebTestCase
     RebuildConfig rebuildConfig = new RebuildConfig();
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex(index);
-    rebuildBackend(backendID, rebuildConfig);
+    backend = DirectoryServer.getBackend(backendID);
+    backend.rebuildBackend(rebuildConfig, getServerContext());
 
     if(index.contains(".") && !index.startsWith("vlv."))
     {
@@ -187,8 +189,8 @@ public class TestRebuildJob extends JebTestCase
     RebuildConfig rebuildConfig = new RebuildConfig();
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex("id2entry");
-    rebuildBackend(backendID, rebuildConfig);
-
+    backend = DirectoryServer.getBackend(backendID);
+    backend.rebuildBackend(rebuildConfig, getServerContext());
   }
 
   /**
@@ -205,10 +207,11 @@ public class TestRebuildJob extends JebTestCase
     RebuildConfig rebuildConfig = new RebuildConfig();
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex("id2entry");
+    backend = DirectoryServer.getBackend(backendID);
     TaskUtils.disableBackend(backendID);
 
     try {
-      rebuildBackend(backendID, rebuildConfig);
+      backend.rebuildBackend(rebuildConfig, getServerContext());
     } finally {
       TaskUtils.enableBackend(backendID);
     }
@@ -221,7 +224,8 @@ public class TestRebuildJob extends JebTestCase
     RebuildConfig rebuildConfig = new RebuildConfig();
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex(index);
-    rebuildBackend(backendID, rebuildConfig);
+    backend = DirectoryServer.getBackend(backendID);
+    backend.rebuildBackend(rebuildConfig, getServerContext());
   }
 
   @Test(dataProvider = "systemIndexes",
@@ -232,7 +236,8 @@ public class TestRebuildJob extends JebTestCase
     RebuildConfig rebuildConfig = new RebuildConfig();
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex(index);
-    rebuildBackend(backendID, rebuildConfig);
+    backend = DirectoryServer.getBackend(backendID);
+    backend.rebuildBackend(rebuildConfig, getServerContext());
   }
 
   @Test(dataProvider = "systemIndexes")
@@ -243,8 +248,9 @@ public class TestRebuildJob extends JebTestCase
     rebuildConfig.setBaseDN(baseDNs[0]);
     rebuildConfig.addRebuildIndex(index);
 
+    backend = DirectoryServer.getBackend(backendID);
     TaskUtils.disableBackend(backendID);
-    rebuildBackend(backendID, rebuildConfig);
+    backend.rebuildBackend(rebuildConfig, getServerContext());
 
     //TODO: Verify dn2uri database as well.
     if (!"dn2uri".equalsIgnoreCase(index))
@@ -290,10 +296,11 @@ public class TestRebuildJob extends JebTestCase
 
   private void rebuildIndexes(RebuildConfig rebuildConfig) throws Exception
   {
+    backend = DirectoryServer.getBackend(backendID);
     TaskUtils.disableBackend(backendID);
     try
     {
-      rebuildBackend(backendID, rebuildConfig);
+      backend.rebuildBackend(rebuildConfig, getServerContext());
       assertEquals(verifyBackend(null), 0);
     }
     finally
@@ -302,10 +309,9 @@ public class TestRebuildJob extends JebTestCase
     }
   }
 
-  private void rebuildBackend(String backendID, RebuildConfig rebuildConfig) throws Exception
+  private ServerContext getServerContext()
   {
-    backend = DirectoryServer.getBackend(backendID);
-    backend.rebuildBackend(rebuildConfig, DirectoryServer.getInstance().getServerContext());
+    return DirectoryServer.getInstance().getServerContext();
   }
 
   @Test